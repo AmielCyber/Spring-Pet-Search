242edd0f52c4909e860de77c86e802606548895a
Amiel Nava <amieln@icloud.com>
2025-05-28 16:55:57 -0700

    Initial Setup

---
+++ src/main/java/amielcyber/petsearch/api/SpringPetSearchApiApplication.java
@@ -0,0 +1,13 @@
+package amielcyber.petsearch.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetSearchApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPetSearchApiApplication.class, args);
+	}
+
+}
